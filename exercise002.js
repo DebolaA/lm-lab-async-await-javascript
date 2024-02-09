805ce5b44c70871b097a70c04fae141389201bee
Debola Adedapo <niikee80@hotmail.com>
2024-02-09 11:22:06 +0000

    exercise 2 done

--- exercise002.js
+++ exercise002.js
@@ -2,11 +2,14 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-const fetchData = (apiEndPoint) => {
-  fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
+const fetchData = async (apiEndPoint) => {
+  try {
+    const apiResponse = await fetch(apiEndPoint);
+    const responseJson = await apiResponse.json();
+    console.log(responseJson);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 fetchData(jsonTypicode);
